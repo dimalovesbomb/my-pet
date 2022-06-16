657782ff5c45d3c274f003a3dcc399b007525bc0
Dmitrii Kupriunin <kupriunin@gmail.com>
2022-06-16 13:58:40 +0400

    added UseMemompage test

---
+++ src/main_content/UseMemo/UseMemo.test.js
@@ -0,0 +1,7 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { withDelay } from '../../setupTests';
+// import { UseMemo } from "./UseMemo";
+
+test('useMemo page', async () => {
+    // render(<UseMemo/>);
+})
