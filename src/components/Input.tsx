aef248d19efbe32788428eacfab37e9bb9fba47d
Dmitrii Kupriunin <kupriunin@gmail.com>
2022-06-21 17:39:09 +0400

    input fix for testing

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,10 +6,13 @@ interface InputProps {
     onChange: any;
     value: string;
     className?: string;
+    testId?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ label, id, onChange, value, className = '' }) => {
+export const Input: React.FC<InputProps> = ({ label, id, onChange, value, className = '', testId = id }) => {
     return (
-        <label className={className}>{label}: <input type="text" value={value} onChange={onChange} id={id} /></label>
+        <label className={className}>{label}: 
+            <input type="text" value={value} onChange={onChange} id={id} data-testid={testId} />
+        </label>
     );
 }
